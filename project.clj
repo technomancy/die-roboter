210297e0fde5bec1821b6bc532c81f946a148b1c
Phil Hagelberg <technomancy@gmail.com>
2012-05-18 09:48:27 -0700

    No more version ranges, geez.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject die-roboter "1.0.0-SNAPSHOT"
   :description "The robots get your work done in an straightforward way."
   :url "https://github.com/technomancy/die-roboter"
-  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [commons-codec "1.5"]
-                 [com.mefesto/wabbitmq "0.1.4"
+                 [com.mefesto/wabbitmq "0.2.1"
                   :exclusions [org.clojure/clojure-contrib]]
                  [org.clojure/tools.logging "0.2.0"]]
   :checksum-deps true)
