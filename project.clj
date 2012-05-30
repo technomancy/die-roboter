43a2772badd662ad63770cf2287d800b106ee9a9
Phil Hagelberg <technomancy@gmail.com>
2012-05-29 21:17:43 -0700

    Add license.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject die-roboter "1.0.0-SNAPSHOT"
   :description "The robots get your work done in an straightforward way."
   :url "https://github.com/technomancy/die-roboter"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [commons-codec "1.5"]
                  [com.mefesto/wabbitmq "0.2.1"]
